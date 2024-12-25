bf4a3c59806863107238aec288f96ff850484f6a
Sanpawat Sewsuwan <koyong555@hotmail.com>
2024-12-25 22:04:32 +0700

    Committed

--- Chrismas Tree/ContentView.swift
+++ Chrismas Tree/ContentView.swift
@@ -51,6 +51,7 @@ struct ContentView: View {
                       .onAppear {
                           isAnimating.toggle()
                       }
+                 
                   
                   // Star on top
                   Image(systemName: "star.fill")
